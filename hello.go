505fe443667948187571b7d9b83cb422901cb482
Patrick Mauro <patrick@mauro.ca>
2019-07-31 20:30:42 -0700

    added flags for secrests and tokens from strava

--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var clientID = flag.Int("clientID", -1, "Client ID found at https://www.strava.com/settings/api")
+var clientSecret = flag.String("clientSecret", "", "Client Secret found at https://www.strava.com/settings/api")
+var refreshToken = flag.String("refreshToken", "", "Refresh token provided by Strava")
 
 func main() {
-    fmt.Println("Hello")
+	flag.Parse()
+
+	fmt.Println("clientId: ", *clientID)
+	fmt.Println("clientSecret: ", *clientSecret)
+	fmt.Println("refreshToken: ", *refreshToken)
 }
