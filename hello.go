6ae6cc09db734b6822ff81d1d757381ec7368791
Patrick Mauro <patrick@mauro.ca>
2019-07-31 21:13:27 -0700

    refreshing the token based on commandline inputs using a simple http post form

--- hello.go
+++ hello.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
 )
 
 var clientID = flag.Int("clientID", -1, "Client ID found at https://www.strava.com/settings/api")
@@ -15,4 +20,25 @@ func main() {
 	fmt.Println("clientId: ", *clientID)
 	fmt.Println("clientSecret: ", *clientSecret)
 	fmt.Println("refreshToken: ", *refreshToken)
+
+	formData := url.Values{
+		"client_id":     {strconv.Itoa(*clientID)},
+		"client_secret": {*clientSecret},
+		"refresh_token": {*refreshToken},
+		"grant_type":    {"refresh_token"},
+	}
+
+	resp, err := http.PostForm("https://www.strava.com/oauth/token", formData)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s\n", string(body))
 }
