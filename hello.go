94325ee9eca55b66ac2e48f0b6c5e276071db0db
Patrick Mauro <patrick@mauro.ca>
2019-08-01 20:57:40 -0700

    Read strava tokens from json file

--- hello.go
+++ hello.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 )
 
@@ -15,7 +17,39 @@ var clientSecret = flag.String("clientSecret", "", "Client Secret found at https
 var refreshToken = flag.String("refreshToken", "", "Refresh token provided by Strava")
 
 func main() {
-	flag.Parse()
+	if len(os.Args) > 1 { // if arguments provided we'll use those
+		flag.Parse()
+	} else { // read the values from the json file instead
+		data, err := ioutil.ReadFile("./tokens.json")
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		// define data structure
+		type Tokens struct { // not, struct members must be capitized, or they're not visible outside the struct
+			ClientID     int
+			ClientSecret string
+			RefreshToken string
+		}
+
+		fmt.Println("data: ", data)
+
+		// json data
+		var obj Tokens
+
+		// unmarshall it
+		err = json.Unmarshal(data, &obj)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+
+		fmt.Println("json: ", obj)
+
+		*clientID = obj.ClientID
+		*clientSecret = obj.ClientSecret
+		*refreshToken = obj.RefreshToken
+
+	}
 
 	fmt.Println("clientId: ", *clientID)
 	fmt.Println("clientSecret: ", *clientSecret)
