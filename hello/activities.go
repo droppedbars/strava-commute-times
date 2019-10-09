9f5ab4a698f23a57eb6cd206d72014ef2a6f8257
Patrick Mauro <patrick@mauro.ca>
2019-10-08 22:13:00 -0700

    help functions to handle reading array and non-array responses from Strava and a basic return of the list of activities in place

--- hello/activities.go
+++ hello/activities.go
@@ -13,8 +13,8 @@ const stravaBasePath = "https://www.strava.com/api/v3/"
 const stravaGetActivityPath = stravaBasePath + "activities/"
 const stravaListActivitiesPath = stravaBasePath + "athlete/activities/"
 
-func stravaAPIGetCall(url string, params map[string]uint64, accessToken string) map[string]interface{} {
-	log.Println("API call URL ", url)
+func stravaAPIGetResponse(url string, params map[string]uint64, accessToken string) []byte {
+	log.Println("Base API call URL ", url)
 
 	client := http.Client{
 		Timeout: time.Duration(5 * time.Second),
@@ -27,7 +27,7 @@ func stravaAPIGetCall(url string, params map[string]uint64, accessToken string) 
 		query.Add(key, strconv.FormatUint(value, 10))
 	}
 	request.URL.RawQuery = query.Encode()
-	log.Println("API call URL ", request.URL.String())
+	log.Println("Full API call URL ", request.URL.String())
 
 	resp, err := client.Do(request)
 	if err != nil {
@@ -42,19 +42,36 @@ func stravaAPIGetCall(url string, params map[string]uint64, accessToken string) 
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln("Error reading body: ", err)
+	}
 
-	//data, err := json.Unmarshal(body)
-	log.Printf("activity body: %s\n", body) // dumps the whole resonse
+	//log.Printf("activity body: %s\n", body) // dumps the whole resonse
+	return body
+}
 
-	//var parsed map[string]interface{}
-	//err = json.Unmarshal(body, &parsed)
-	//if err != nil {
-	//	//	return auth, err
-	//	log.Fatalln("Unable to read the activities get: ", err)
-	//}
+func stravaAPIGetJSON(url string, params map[string]uint64, accessToken string) map[string]interface{} {
+	rawResponse := stravaAPIGetResponse(url, params, accessToken)
 
-	//return parsed
-	return nil
+	var parsed map[string]interface{}
+	err := json.Unmarshal(rawResponse, &parsed)
+	if err != nil {
+		log.Fatalln("Unable to parse the response: ", err)
+	}
+
+	return parsed
+}
+
+func stravaAPIGetArray(url string, params map[string]uint64, accessToken string) []map[string]interface{} {
+	rawResponse := stravaAPIGetResponse(url, params, accessToken)
+
+	var parsed []map[string]interface{}
+	err := json.Unmarshal(rawResponse, &parsed)
+	if err != nil {
+		log.Fatalln("Unable to parse the response: ", err)
+	}
+
+	return parsed
 }
 
 func getActivities(startDate uint64, endDate uint64, accessToken string) {
@@ -62,49 +79,20 @@ func getActivities(startDate uint64, endDate uint64, accessToken string) {
 		"before": endDate,
 		"after":  startDate,
 		//"page": 1,
-		"per_page": 200,
+		"per_page": 2,
 	}
-	jsonResponse := stravaAPIGetCall(stravaListActivitiesPath, activitiyListParams, accessToken)
+	arrayJSONResponse := stravaAPIGetArray(stravaListActivitiesPath, activitiyListParams, accessToken)
 
-	log.Println(jsonResponse)
+	log.Println("API call response: ", arrayJSONResponse)
+	log.Println("\n\nOne response: ", arrayJSONResponse[1])
+	log.Println("\n\nOne response's name: ", arrayJSONResponse[1]["name"].(string))
 }
 
 func outputActivityStartStop(id uint64, accessToken string) {
-	url := stravaGetActivityPath + strconv.FormatUint(id, 10)
-	log.Println("activity URL ", url)
-
-	client := http.Client{
-		Timeout: time.Duration(5 * time.Second),
-	}
-	request, err := http.NewRequest("GET", url, nil)
-	request.Header.Set("Authorization", "Bearer "+accessToken)
-
-	resp, err := client.Do(request)
-	if err != nil {
-		//return auth, err
-		log.Fatalln("Unable to access the activities get: ", err)
-	}
-	defer resp.Body.Close()
-
-	// ensure a proper response. Anything other than 200 is an error (user or server)
-	if resp.StatusCode != 200 {
-		log.Fatalf("HTTP Status not 200: %d - %s", resp.StatusCode, resp.Status)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	//data, err := json.Unmarshal(body)
-	//log.Printf("activity body: %s\n", body) // dumps the whole resonse
-
-	var parsed map[string]interface{}
-	err = json.Unmarshal(body, &parsed)
-	if err != nil {
-		//	return auth, err
-		log.Fatalln("Unable to read the activities get: ", err)
-	}
+	JSONResponse := stravaAPIGetJSON(stravaGetActivityPath+strconv.FormatUint(id, 10), nil, accessToken)
 
 	// update the token struct with the new refresh token from Strava OAuth request
-	name := parsed["name"].(string)
+	name := JSONResponse["name"].(string)
 
 	log.Println("activity name: ", name)
 }
