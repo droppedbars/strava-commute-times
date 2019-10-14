9d60f9a208d90d9baf3e27fe3888e77bd7240408
Patrick Mauro <patrick@mauro.ca>
2019-10-14 14:38:37 -0700

    refactor: move data processing functions to hello.go and keep activities.go for calling strava APIs

--- hello/hello.go
+++ hello/hello.go
@@ -15,6 +15,7 @@ import (
 	"net/url"
 	"os"
 	"strconv"
+	"time"
 )
 
 const tokenJSONFileName = "./tokens.json"
@@ -207,6 +208,53 @@ func stravaOAuthCall(sec secrets, grantType string, auth tokens) (tokens, error)
 	return auth, nil
 }
 
+func outputActivityStartStop(id uint64, accessToken string) {
+	JSONResponse := stravaAPIGetJSON(stravaGetActivityPath+strconv.FormatUint(id, 10), nil, accessToken)
+
+	name := JSONResponse["name"].(string)
+
+	log.Println("activity name: ", name)
+}
+
+func outputDistanceTotals(allActivities []map[string]interface{}) (float64, float64) {
+	commute := 0.0
+	total := 0.0
+
+	for j := 0; j < len(allActivities); j++ {
+		//log.Println("Activity Name: ", allActivities[j]["name"].(string))
+		if allActivities[j]["type"] == "Ride" || allActivities[j]["type"] == "EBikeRide" {
+			distance := allActivities[j]["distance"].(float64) / 1000 // convert m to km
+			total += distance
+			if allActivities[j]["commute"].(bool) == true {
+				commute += distance
+			}
+		}
+	}
+	return total, commute
+}
+
+func loadActivities(startDate uint64, endDate uint64, accessToken string) []map[string]interface{} {
+	var allActivities []map[string]interface{}
+
+	for i := 1; ; i++ { // strava pages start at 1
+		activitiyListParams := map[string]uint64{
+			"before":   endDate,
+			"after":    startDate,
+			"page":     uint64(i),
+			"per_page": 200,
+		}
+		arrayJSONResponse := stravaAPIGetArray(stravaListActivitiesPath, activitiyListParams, accessToken)
+		if len(arrayJSONResponse) == 0 { // empty response, so no more data
+			break
+		}
+		allActivities = append(allActivities, arrayJSONResponse...)
+		//log.Println("Page: ", i)
+		//log.Println("API call response page: "+strconv.Itoa(i)+": ", arrayJSONResponse)
+		//log.Println("\n\nOne response: ", arrayJSONResponse[1])
+	}
+	return allActivities
+}
+
 // main execution function.
 func main() {
 	var auth tokens
@@ -229,9 +277,26 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	outputActivityStartStop(2685947039, auth.AccessToken)
+	//outputActivityStartStop(2685947039, auth.AccessToken)
 
 	//"before": 1577865599, //December 31, 2019 11:59:59 PM GMT-08:00
-	//"after": 1546329601, //January 1, 2019 12:00:01 AM GMT-08:00
-	getActivities(1546329601, 1577865599, auth.AccessToken)
+	//"after": 1546329601, //January 1, 2019 12:00:01 AM GMT-08:
+	var startTime time.Time
+	var endTime time.Time
+	startTime, err = time.Parse(time.RFC3339, "2019-01-01T12:00:01-08:00")
+	endTime, err = time.Parse(time.RFC3339, "2019-12-31T11:59:59-08:00")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Start Time: ", startTime)
+	log.Println("End Time: ", endTime)
+
+	allActivities := loadActivities(uint64(startTime.Unix()), uint64(endTime.Unix()), auth.AccessToken)
+	total, commute := outputDistanceTotals(allActivities)
+	log.Printf("Total Distance (km): %.1f\n", total)
+	log.Printf("Total Commute (km): %.1f, %.1f%%\n", commute, (commute/total)*100)
+	log.Printf("Total Pleasure (km): %.1f, %.1f%%\n", total-commute, ((total-commute)/total)*100)
+
 }
