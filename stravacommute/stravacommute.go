f45d011953ea86fe2f1c413a81e8fdd6f88bf068
Patrick Mauro <patrick@mauro.ca>
2019-10-26 14:16:11 -0700

    get the numbers for a range of years, bounded by when Strava launched and the current year

--- stravacommute/stravacommute.go
+++ stravacommute/stravacommute.go
@@ -15,8 +15,10 @@ import (
 
 const annualCommuteKm = 5875 // assumes 25km/day, 5 days a week, 5 weeks of no riding per year
 const hoursInYear = 24 * 365 // simplistic, ignores leap years
+const epoch = 2009           // when strava started, so there should never be data before this
 
-var flagYear = flag.Int("year", time.Now().Year(), "Year to run the commute numbers for. Defaults to current year.")
+var flagYear1 = flag.Int("startYear", time.Now().Year(), "First year to run the commute numbers for. Defaults to current year.")
+var flagYear2 = flag.Int("endYear", time.Now().Year(), "Last year to run the commute numbers for. Defaults to current year.")
 
 // outputActivityStartStop will take the id of a Strava activity and print out the times
 // the activity started and stopped. It uses the accessToken to make a API call to Strava.
@@ -82,6 +84,24 @@ func getRidingActivities(startDate uint64, endDate uint64, accessToken string) [
 func main() {
 	setLogging(true, debugLevel)
 
+	flag.Parse()
+	var year1 int
+	var year2 int
+
+	year1 = *flagYear1
+	year2 = *flagYear2
+	if year1 > year2 {
+		year2 = *flagYear1
+		year1 = *flagYear2
+	}
+	currentYear := time.Now().Year()
+	if year2 > currentYear {
+		year2 = currentYear
+	}
+	if year1 < epoch {
+		year1 = epoch
+	}
+
 	var auth tokens
 
 	sec, err := loadSecrets()
@@ -102,40 +122,41 @@ func main() {
 		ERROR.Fatalln(err)
 	}
 
-	flag.Parse()
-
 	//outputActivityStartStop(2685947039, auth.AccessToken)
 
-	// TODO too early of a year such as 1900 or late such as 9999 will return a 400 error from Strava. Best to parse it out early.
-	year := strconv.Itoa(*flagYear)
-	var startTime time.Time
-	var endTime time.Time
-	startTime, err = time.Parse(time.RFC3339, year+"-01-01T12:00:01-08:00")
-	endTime, err = time.Parse(time.RFC3339, year+"-12-31T11:59:59-08:00")
+	for i := year1; i <= year2; i++ {
+		year := strconv.Itoa(i)
 
-	if err != nil {
-		ERROR.Fatalln(err)
-	}
+		var startTime time.Time
+		var endTime time.Time
+		startTime, err = time.Parse(time.RFC3339, year+"-01-01T12:00:01-08:00")
+		endTime, err = time.Parse(time.RFC3339, year+"-12-31T11:59:59-08:00")
 
-	INFO.Println("Commute time range start: ", startTime)
-	INFO.Println("Commute time range end: ", endTime)
+		if err != nil {
+			ERROR.Fatalln(err)
+		}
 
-	allActivities := getRidingActivities(uint64(startTime.Unix()), uint64(endTime.Unix()), auth.AccessToken)
-	total, commute := ridingDistanceTotals(allActivities)
-	percentageOfYear := 1.0
-	fullYear := true
-	if endTime.Unix() > time.Now().Unix() {
-		percentageOfYear = time.Since(startTime).Hours() / hoursInYear
-		fullYear = false
+		INFO.Println("Commute time range start: ", startTime)
+		INFO.Println("Commute time range end: ", endTime)
+
+		allActivities := getRidingActivities(uint64(startTime.Unix()), uint64(endTime.Unix()), auth.AccessToken)
+		total, commute := ridingDistanceTotals(allActivities)
+		percentageOfYear := 1.0
+		fullYear := true
+		if endTime.Unix() > time.Now().Unix() {
+			percentageOfYear = time.Since(startTime).Hours() / hoursInYear
+			fullYear = false
+		}
+		fmt.Println(year)
+		fmt.Printf("Total Distance (km): %.1f\n", total)
+		if !fullYear {
+			fmt.Printf("  Estimated end of year distance (km): %.1f\n", total/percentageOfYear)
+		}
+		fmt.Printf("Total Commute (km): %.1f, %.1f%%\n", commute, (commute/total)*100)
+		fmt.Printf("  Percentage of commute by bike: %.1f%%\n", (commute/annualCommuteKm)*100)
+		if !fullYear {
+			fmt.Printf("  Estimated percentage of commute by bike for year: %.1f%%\n", (commute/annualCommuteKm/percentageOfYear)*100)
+		}
+		fmt.Printf("Total Pleasure (km): %.1f, %.1f%%\n", total-commute, ((total-commute)/total)*100)
 	}
-	fmt.Printf("Total Distance (km): %.1f\n", total)
-	if !fullYear {
-		fmt.Printf("  Estimated end of year distance (km): %.1f\n", total/percentageOfYear)
-	}
-	fmt.Printf("Total Commute (km): %.1f, %.1f%%\n", commute, (commute/total)*100)
-	fmt.Printf("  Percentage of commute by bike: %.1f%%\n", (commute/annualCommuteKm)*100)
-	if !fullYear {
-		fmt.Printf("  Estimated percentage of commute by bike for year: %.1f%%\n", (commute/annualCommuteKm/percentageOfYear)*100)
-	}
-	fmt.Printf("Total Pleasure (km): %.1f, %.1f%%\n", total-commute, ((total-commute)/total)*100)
 }
