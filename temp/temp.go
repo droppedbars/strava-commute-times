ae294d078994429959a4fb1e90427940ce6b2415
Patrick Mauro <patrick@mauro.ca>
2019-11-25 13:54:11 -0800

    removed the temp package that was used just for testing out packages and godoc for packages

--- temp/temp.go
+++
@@ -1,9 +0,0 @@
-// Package temp is a test for making sure I understand how godoc works for documentation
-package temp
-
-import "fmt"
-
-// Wonder must do something, right? It's public, so the doc should appear!
-func Wonder() {
-	fmt.Println("wow, what a wonder")
-}
